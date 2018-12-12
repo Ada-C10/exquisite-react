aa41961dfea379cc4f039c6aeb327151950b87d7
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-12-12 12:52:30 -0800

    Add form css style for invalid input with regex

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -16,6 +16,30 @@ class PlayerSubmissionForm extends Component {
     }
   }
 
+  adjective1Valid = () => {
+    return /^[a-zA-Z]+$/.test(this.state.adjective1)
+  }
+
+  noun1Valid = () => {
+    return /^[a-zA-Z]+$/.test(this.state.noun1)
+  }
+
+  adverbValid = () => {
+    return /^[a-zA-Z]+$/.test(this.state.adverb)
+  }
+
+  verbValid = () => {
+    return /^[a-zA-Z]+$/.test(this.state.verb)
+  }
+
+  adjective2Valid = () => {
+    return /^[a-zA-Z]+$/.test(this.state.adjective2)
+  }
+
+  noun2Valid = () => {
+    return /^[a-zA-Z]+$/.test(this.state.noun2)
+  }
+
   onAdjective1Change = (event) => {
     console.log('adjective1 update')
     this.setState({
@@ -91,36 +115,42 @@ class PlayerSubmissionForm extends Component {
               onChange={this.onAdjective1Change}
               value={this.state.adjective1}
               placeholder="adjective"
+              className={this.adjective1Valid() ? "input": "PlayerSubmissionFormt__input--invalid"}
               name="adjective1" />
 
             <input
               onChange={this.onNoun1Change}
               value={this.state.noun1}
               placeholder="noun"
+              className={this.noun1Valid() ? "valid": "PlayerSubmissionFormt__input--invalid"}
               name="noun1" />
 
             <input
               onChange={this.onAdverbChange}
               value={this.state.adverb}
               placeholder="adverb"
+              className={this.adverbValid() ? "valid": "PlayerSubmissionFormt__input--invalid"}
               name="adverb" />
 
             <input
               onChange={this.onVerbChange}
               value={this.state.verb}
               placeholder="verb"
+              className={this.verbValid() ? "valid": "PlayerSubmissionFormt__input--invalid"}
               name="verb" />
 
             <input
               onChange={this.onAdjective2Change}
               value={this.state.adjective2}
               placeholder="adjective"
+              className={this.adjective2Valid() ? "input": "PlayerSubmissionFormt__input--invalid"}
               name="adjective2" />
 
             <input
               onChange={this.onNoun2Change}
               value={this.state.noun2}
               placeholder="noun"
+              className={this.noun2Valid() ? "valid": "PlayerSubmissionFormt__input--invalid"}
               name="noun2" />
 
           </div>
