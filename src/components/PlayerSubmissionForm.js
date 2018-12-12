1977b8d111bbacba31320f503f6208c925d27a9c
Valerie Gidzinski <valgidzi@gmail.com>
2018-12-11 16:32:08 -0800

    Basic submission form, onInputChange event handler

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -5,6 +5,18 @@ class PlayerSubmissionForm extends Component {
 
   constructor(props) {
     super(props);
+
+
+  }
+
+  onInputChange = (event) => {
+    console.log('On Input Change');
+    const field = event.target.name;
+    const value = event.target.value;
+
+    const newState = {};
+    newState[field] = value;
+    this.setState(newState);
   }
 
   render() {
@@ -17,13 +29,38 @@ class PlayerSubmissionForm extends Component {
 
           <div className="PlayerSubmissionForm__poem-inputs">
 
-            {
-              // Put your form inputs here... We've put in one below as an example
-            }
+            The
             <input
-              placeholder="hm..."
-              type="text" />
-
+              placeholder="adjective"
+              type="text"
+              name="firstAdjective"
+              onChange={this.onInputChange}/>
+            <input
+              placeholder="noun"
+              type="text"
+              name="firstNoun"
+              onChange={this.onInputChange} />
+            <input
+              placeholder="adverb"
+              type="text"
+              name="adverb"
+              onChange={this.onInputChange} />
+            <input
+              placeholder="verb"
+              type="text"
+              name="verb"
+              onChange={this.onInputChange} />
+            the
+            <input
+              placeholder="adjective"
+              type="text"
+              name="secondAdjective"
+              onChange={this.onInputChange} />
+            <input
+              placeholder="noun"
+              type="text"
+              name="secondNoun"
+              onChange={this.onInputChange} /> .
           </div>
 
           <div className="PlayerSubmissionForm__submit">
