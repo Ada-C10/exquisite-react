c99b8d429b74f40555e35285a98be678ced9afb5
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-12-12 11:48:10 -0800

    Create two different display screens in Game.js to handle final poem reveal screen

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -2,10 +2,7 @@ import React from 'react';
 import './FinalPoem.css';
 
 
-
-const FinalPoem = (props) => {
-
-  const poem = props.poem.map(function(line, key) {
+const displayPoem = (poem) => poem.map(function(line, key) {
     return (
       <span key={key}>
         {line}
@@ -13,15 +10,25 @@ const FinalPoem = (props) => {
         <br/>
       </span>
     )
-  })
+})
 
-  return (
-    <div className="FinalPoem">
+
+const FinalPoem = (props) => {
+
+  const revealPoem = () => {
+    return (
       <section className="FinalPoem__poem">
         <h3>Final Poem</h3>
-          {poem}
+          {displayPoem(props.poem)}
 
       </section>
+    )
+  }
+
+  console.log(props.poem)
+  return (
+    <div className="FinalPoem">
+      {props.poem === false ? "" : revealPoem()}
       <div className="FinalPoem__reveal-btn-container">
         <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
       </div>
--- src/components/Game.js
+++ src/components/Game.js
@@ -10,7 +10,8 @@ class Game extends Component {
     super(props);
 
     this.state = {
-      poemLines: []
+      poemLines: [],
+      endGameScreen: false
     }
   }
 
@@ -27,6 +28,12 @@ class Game extends Component {
     return this.state.poemLines.length + 1
   }
 
+  revealFinalScreen = () => {
+    this.setState({
+      endGameScreen: true
+    })
+  }
+
 
   render() {
 
@@ -38,28 +45,54 @@ class Game extends Component {
       }
     }).join(" ");
 
+
+    const displayAllContent = (
+        <section>
+          <h2>Game</h2>
+
+          <p>Each player should take turns filling out and submitting the form below. Each turn should be done individually and <em>in secret!</em> Take inspiration from the revealed recent submission. When all players are finished, click the final button on the bottom to reveal the entire poem.</p>
+
+          <p>Please follow the following format for your poetry submission:</p>
+
+          <p className="Game__format-example">
+            { exampleFormat }
+          </p>
+
+          <RecentSubmission
+            poemLine={this.state.poemLines.slice(-1)[0]}/>
+
+          <PlayerSubmissionForm
+            addPoemLineCallback={this.addPoemLine}
+            getLineCountCallback={this.getLineCount}/>
+
+          <FinalPoem
+            poem={false}/>
+
+        </section>
+    );
+
+
+    const displayEndGame = (
+        <section>
+          <h2>Game</h2>
+
+          <p>Each player should take turns filling out and submitting the form below. Each turn should be done individually and <em>in secret!</em> Take inspiration from the revealed recent submission. When all players are finished, click the final button on the bottom to reveal the entire poem.</p>
+
+          <p>Please follow the following format for your poetry submission:</p>
+
+          <p className="Game__format-example">
+            { exampleFormat }
+          </p>
+
+          <FinalPoem
+            poem={this.state.poemLines} />
+
+        </section>
+    );
+
     return (
       <div className="Game">
-        <h2>Game</h2>
-
-        <p>Each player should take turns filling out and submitting the form below. Each turn should be done individually and <em>in secret!</em> Take inspiration from the revealed recent submission. When all players are finished, click the final button on the bottom to reveal the entire poem.</p>
-
-        <p>Please follow the following format for your poetry submission:</p>
-
-        <p className="Game__format-example">
-          { exampleFormat }
-        </p>
-
-        <RecentSubmission
-          poemLine={this.state.poemLines.slice(-1)[0]}/>
-
-        <PlayerSubmissionForm
-          addPoemLineCallback={this.addPoemLine}
-          getLineCountCallback={this.getLineCount}/>
-
-        <FinalPoem
-          poem={this.state.poemLines}/>
-
+      {this.state.endGameScreen ? displayEndGame : displayAllContent}
       </div>
     );
   }
