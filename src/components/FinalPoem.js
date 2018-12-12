ce2e0f83f5a885b181b7f343eea9d193a8cb7c11
Katrina Aganon <katrina.aganon@gmail.com>
2018-12-11 18:05:38 -0800

    add onclick event handler for final poem

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -3,18 +3,37 @@ import './FinalPoem.css';
 
 const FinalPoem = (props) => {
 
-  return (
-    <div className="FinalPoem">
-      <section className="FinalPoem__poem">
-        <h3>Final Poem</h3>
+  const changePoemStatus = () => {
+    console.log("status:", props.finalPoemStatus);
+    props.changePoemStatus();
+  };
 
-      </section>
 
-      <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
-      </div>
+  const lines = props.lines;
+
+  const poem = lines.map((line, i) => {
+    if (props.finalPoemStatus) {
+      return <ul>{line}</ul>
+    };
+  });
+
+
+
+return (
+  <div className="FinalPoem">
+    <section className="FinalPoem__poem">
+      <h3>Final Poem</h3>
+      <ul>
+        {poem}
+      </ul>
+
+    </section>
+
+    <div className="FinalPoem__reveal-btn-container">
+      <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" onClick={changePoemStatus} />
     </div>
-  );
+  </div>
+);
 }
 
 export default FinalPoem;
