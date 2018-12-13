a8195a262e1d622ea34a1c2bb9e5f005fe5cf45d
Semret Nicodimos <snicodimos@gmail.com>
2018-12-12 19:23:00 -0800

    added a callback function and changed state of status

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -2,16 +2,38 @@ import React from 'react';
 import './FinalPoem.css';
 
 const FinalPoem = (props) => {
+    console.log(props);
+    const displayFinalLines = (props) => {
+      if (props.status === true){
+        if (props !== undefined){
+
+          let poemLines = "";
+          props.finalLines.forEach((line, i) => {
+
+            const linesForFinal = `The ${line.adjective1}  ${line.noun1}  ${line.adverb}  ${line.verb} the  ${line.adjective2}   ${line.noun2}`;
+
+            poemLines += linesForFinal;
+
+          })
+          return poemLines;
+        };
+      }
+    }
+    // const onClickDisplay = (event) => {
+    //   props.callbackDisplayPoem();
+    // }
 
   return (
     <div className="FinalPoem">
       <section className="FinalPoem__poem">
         <h3>Final Poem</h3>
-
+        <div>{displayFinalLines(props)}</div>
       </section>
 
       <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
+        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn"
+            onClick={()=> props.callbackDisplayPoem()}
+            />
       </div>
     </div>
   );
