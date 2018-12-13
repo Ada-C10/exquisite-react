eceffb0efb96d061a29e6f4696af3900a642c001
Chantelle Belic <chantelle.belic@gmail.com>
2018-12-12 21:30:26 -0800

    mad conditional/ternary more declarative

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -3,16 +3,14 @@ import './FinalPoem.css';
 import PropTypes from 'prop-types';
 
 const FinalPoem = (props) => {
-  let poem = ""
-  props.ended ? poem = props.poem.join('') : poem = ""
+  const poem = props.poem.join('')
 
   return (
     <div className="FinalPoem">
       <section className="FinalPoem__poem">
         <h3>Final Poem</h3>
       </section>
-      <p>{poem}</p>
-
+      { props.ended &&  (<p>{poem}</p>) }
       <div className="FinalPoem__reveal-btn-container">
         <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" onClick={props.revealPoemCallback} />
       </div>
