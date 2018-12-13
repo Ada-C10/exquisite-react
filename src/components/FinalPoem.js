a4c0fb494283e0e531e277d92291be4bad21e098
Addison Cole <addison.cole85@gmail.com>
2018-12-12 19:13:54 -0800

    create submission form and pass callback from game to submission form to take submission and add the line of poetry to the array state of the poem in game.

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import './FinalPoem.css';
 
-const FinalPoem = (props) => {
+class FinalPoem extends React.Component {
 
-  return (
-    <div className="FinalPoem">
-      <section className="FinalPoem__poem">
-        <h3>Final Poem</h3>
+  constructor(props) {
+    super(props);
 
-      </section>
+    this.state = {
+      cadaver: "",
+    }
+  }
 
-      <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
+  composeCadaver = (e) => {
+    console.log("The Mark Present Button was clicked!");
+    const cadaver = this.props.finalPoemCallback();
+    this.setState({
+      cadaver: cadaver
+    });
+  }
+  render() {
+    return (
+      <div className="FinalPoem">
+        <section className="FinalPoem__poem">
+          <h3>Final Poem</h3>
+            {this.state.cadaver}
+        </section>
+
+        <div className="FinalPoem__reveal-btn-container">
+          <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" onClick={this.composeCadaver}/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
 
 export default FinalPoem;
