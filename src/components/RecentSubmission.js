10ad6c4ec98237a55924b0c94c190007280b4223
Semret Nicodimos <snicodimos@gmail.com>
2018-12-12 17:17:15 -0800

    added a function to display line in recent sub

--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './RecentSubmission.css';
 
 const RecentSubmission = (props) => {
+  console.log(props);
+  const displayLine = (props) => {
+    if (props != undefined){
+      const line = `The ${props.adjective1}  ${props.noun1}  ${props.adverb}  ${props.verb} the  ${props.adjective2}   ${props.noun2}`;
+      return line
+    };
+  }
+
   return (
     <div className="RecentSubmission">
       <h3>The Most Recent Submission</h3>
-      <p className="RecentSubmission__submission">{  }</p>
+      <p className="RecentSubmission__submission">{ displayLine(props.recentLine) }</p>
     </div>
   );
 }
