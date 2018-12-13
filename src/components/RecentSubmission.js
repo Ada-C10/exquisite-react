f13a64b9fb6f59b6661ccda141a765ecee5507e7
Semret Nicodimos <snicodimos@gmail.com>
2018-12-12 19:24:18 -0800

    minor changes in functional names

--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -3,6 +3,7 @@ import './RecentSubmission.css';
 
 const RecentSubmission = (props) => {
   console.log(props);
+
   const displayLine = (props) => {
     if (props != undefined){
       const line = `The ${props.adjective1}  ${props.noun1}  ${props.adverb}  ${props.verb} the  ${props.adjective2}   ${props.noun2}`;
