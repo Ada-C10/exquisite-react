55ae6dfdb60d2aafb436f3f111a0f80446380189
kangazoom <michellelcronin@gmail.com>
2018-12-13 16:15:57 -0800

    started clean up + fixed bug where recent submission header was always shown

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,13 +4,6 @@ import PlayerSubmissionForm from './PlayerSubmissionForm';
 import FinalPoem from './FinalPoem';
 import RecentSubmission from './RecentSubmission';
 
-// TODO:
-// - unique key?
-// DRY code
-// HTML?? <p></p>
-// idea - only show poemDraft when user submits that button to see the whole thing
-
-
 class Game extends Component {
 
   constructor(props) {
@@ -21,129 +14,98 @@ class Game extends Component {
       showPoem: false,
       showPoemRevealButton: false
     };
-  }
+  };
 
   formatLine = (line) => {
     let {adjective1, noun1, adverb1, verb1, adjective2, noun2} = line;
 
     return `The ${adjective1} ${noun1} ${adverb1} ${verb1} the ${adjective2} ${noun2} .`
-  }
+  };
 
   addPoemLine = (poemLine) => {
     const poemLines = this.state.poemLines;
     poemLines.push(this.formatLine(poemLine));
 
     this.setState({poemLines: poemLines});
-    this.setState({showPoemRevealButton: true})
-
-    this.findRecentLine(poemLine)
-
-  }
+    this.setState({showPoemRevealButton: true});
+  };
 
   findRecentLine = (poemLine) => {
-    return poemLine;
-  }
+    return poemLine
+  };
 
   revealPoemHandler = () => {
-    this.setState({
-      showPoem: true
-    });
-  }
+    this.setState({showPoem: true});
+  };
+
+  render() {
+
+    let showPoem = this.state.showPoem;
+    let poemLines = this.state.poemLines;
+    let numLines = (poemLines.length);
+    let userNum = numLines+1;
+
+    // const exampleFormat = FIELDS.map((field) => {
+    //   if (field.key) {
+    //     return field.placeholder;
+    //   } else {
+    //     return field;
+    //   }
+    // }).join(" ");
+
+    return (
+      <div className="Game">
+        <h2>Game</h2>
+
+        <p>Each player should take turns filling out and submitting the form below. Each turn should be done individually and <em>in secret!</em> Take inspiration from the revealed recent submission. When all players are finished, click the final button on the bottom to reveal the entire poem.</p>
+
+        <p>Please follow the following format for your poetry submission:</p>
+
+        <p className="Game__format-example">The adjective noun adverb verb the adjective noun .</p>
 
 
+        {!showPoem && (numLines>0) ? <RecentSubmission poemDraft={this.state.poemLines} /> : ''}
 
+        {!showPoem ? <PlayerSubmissionForm addPoemLineCB={this.addPoemLine}
+        userNum={userNum}/> : ''}
 
-    render() {
-
-      let showPoem = this.state.showPoem
-      let poemLines = this.state.poemLines;
-      let numLines = (poemLines.length)
-      let userNum = numLines+1
-
-
-
-      // let poemDraft = () => {
-      //
-      // poemLines.map((poemLine, i) => {
-      //   return <FinalPoem
-      //     key = {i}
-      //     index = {i}
-      //     adjective1 = {poemLine.adjective1}
-      //     noun1 = {poemLine.noun1}
-      //     adverb1 = {poemLine.adverb1}
-      //     verb1 = {poemLine.verb1}
-      //     adjective2 = {poemLine.adjective2}
-      //     noun2 = {poemLine.noun2}
-      //     />
-      // );}}
-
-
-
-
-
-
-
-      // const exampleFormat = FIELDS.map((field) => {
-      //   if (field.key) {
-      //     return field.placeholder;
-      //   } else {
-      //     return field;
-      //   }
-      // }).join(" ");
-
-      return (
-        <div className="Game">
-          <h2>Game</h2>
-
-          <p>Each player should take turns filling out and submitting the form below. Each turn should be done individually and <em>in secret!</em> Take inspiration from the revealed recent submission. When all players are finished, click the final button on the bottom to reveal the entire poem.</p>
-
-          <p>Please follow the following format for your poetry submission:</p>
-
-          <p className="Game__format-example">The adjective noun adverb verb the adjective noun .</p>
-
-
-          {!showPoem ? <RecentSubmission poemDraft={this.state.poemLines} /> : ''}
-
-          {!showPoem ? <PlayerSubmissionForm addPoemLineCB={this.addPoemLine}
-            userNum={userNum}/> : ''}
-
-           <FinalPoem poemDraft={this.state.poemLines} showPoem={showPoem}
+        <FinalPoem poemDraft={this.state.poemLines} showPoem={showPoem}
           revealPoemCB={this.revealPoemHandler}
           showPoemRevealButton={this.state.showPoemRevealButton} />
 
-        </div>
-      );
-    }
+      </div>
+    );
   }
+}
 
-  // const FIELDS = [
-  //   "The",
-  //   {
-  //     key: 'adj1',
-  //     placeholder: 'adjective',
-  //   },
-  //   {
-  //     key: 'noun1',
-  //     placeholder: 'noun',
-  //   },
-  //   {
-  //     key: 'adv',
-  //     placeholder: 'adverb',
-  //   },
-  //   {
-  //     key: 'verb',
-  //     placeholder: 'verb',
-  //   },
-  //   "the",
-  //   {
-  //     key: 'adj2',
-  //     placeholder: 'adjective',
-  //   },
-  //   {
-  //     key: 'noun2',
-  //     placeholder: 'noun',
-  //   },
-  //   ".",
-  // ];
+// const FIELDS = [
+//   "The",
+//   {
+//     key: 'adj1',
+//     placeholder: 'adjective',
+//   },
+//   {
+//     key: 'noun1',
+//     placeholder: 'noun',
+//   },
+//   {
+//     key: 'adv',
+//     placeholder: 'adverb',
+//   },
+//   {
+//     key: 'verb',
+//     placeholder: 'verb',
+//   },
+//   "the",
+//   {
+//     key: 'adj2',
+//     placeholder: 'adjective',
+//   },
+//   {
+//     key: 'noun2',
+//     placeholder: 'noun',
+//   },
+//   ".",
+// ];
 
-  export default Game;
+export default Game;
