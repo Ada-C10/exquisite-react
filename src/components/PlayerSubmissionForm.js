8275a964a0b8358e265185e2535c61b60e2e34db
Chantelle Belic <chantelle.belic@gmail.com>
2018-12-26 16:43:38 -0800

    change render function to include programmatically creating input fields and settign classname

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -6,8 +6,7 @@ class PlayerSubmissionForm extends Component {
 
   constructor(props) {
     super(props);
-    this.state = this.fetchFields()
-    console.log(this.state)
+    this.state = this.fetchFields();
   }
 
   fetchFields = () => {
@@ -16,12 +15,9 @@ class PlayerSubmissionForm extends Component {
     });
 
     const startingState = fields.reduce((acc, field) => {
-      console.log(field.key)
       acc[field.key] = ["", field.placeholder];
       return acc
     }, {});
-
-    startingState.playerNum = this.props.playerNum;
 
     return startingState;
   }
@@ -30,56 +26,31 @@ class PlayerSubmissionForm extends Component {
 
     const field = event.target.name;
     const value = event.target.value;
-
-    const updateState = this.state;
-    updateState[field] = value;
-    this.setState(updateState);
+    const updatedState = this.state;
+    updatedState[field][0] = value;
+    this.setState(updatedState);
   }
 
   onLineSubmit = (event) => {
     event.preventDefault();
 
-    this.props.currentLineCallback(`The ${this.state.adj1} ${this.state.noun1} ${this.state.adv} ${this.state.verb} ${this.state.adj2} ${this.state.noun2}.`)
+    this.props.currentLineCallback(`The ${this.state.adj1[0]} ${this.state.noun1[0]} ${this.state.adv[0]} ${this.state.verb[0]} ${this.state.adj2[0]} ${this.state.noun2[0]}.`);
 
-
-
-    this.setState({
-      adj1: "",
-      noun1: "",
-      adv: "",
-      verb: "",
-      adj2: "",
-      noun2: ""
-    });
+    this.setState( this.fetchFields() );
   }
 
   render() {
 
-    // const filteredInputs = this.fetchFields().forEach((input) => {
-    //   // const key = input.key
-    //   console.log("state", this.state[input.key])
-    //
-    //   // const key =
-    //   //       // const key =  this.state[Object.keys(input)[0]]
-    //   //       console.log("state", Object.keys(input)[0])
-    //   console.log("state", Object.keys(input)[0])
-    //   return(
-    //     <input name={input.key} placeholder={input.placeholder} type="text" value={this.state[input.key]} onChange={this.onInputChange}/>
-    //     <input name={input[Object.keys(input)[0]]} placeholder={input.placeholder} type="text" value={input[Object.keys(input)[0]]} onChange={this.onInputChange}/>
-    //   )
-    // })
-    //
     const chooseInputClass = (currentState) => {
-      if (currentState === "") {
-        return "PlayerSubmissionForm__input--invalid"
-      }
-    }
+      return currentState ? "PlayerSubmissionForm__input--invalid" : "";
+    };
 
-    const adj1Class = chooseInputClass(this.state.adj1)
-    const noun1Class = chooseInputClass(this.state.noun1)
-    const advClass = chooseInputClass(this.state.adv)
-    const verbClass = chooseInputClass(this.state.verb)
-    const noun2Class = chooseInputClass(this.state.noun2)
+    const filteredInputs = Object.keys(this.state).map( (key) => {
+      const className = chooseInputClass(this.state[key][0]);
+      return(
+        <input name={key} className={className} placeholder={this.state[key][1]} type="text" value={this.state[key][0]} onChange={this.onInputChange}/>
+      )
+    });
 
     return (
       <div className="PlayerSubmissionForm">
@@ -88,19 +59,8 @@ class PlayerSubmissionForm extends Component {
           { !this.props.ended &&
             ( <form className="PlayerSubmissionForm__form" onSubmit={this.onLineSubmit}>
             <div className="PlayerSubmissionForm__poem-inputs">
-              <input
-                className={adj1Class}
-                name="adj1" placeholder="adjective" type="text" value={this.state.adj1} onChange={this.onInputChange}/>
-              <input name="noun1"
-                className={noun1Class} placeholder="noun" type="text" value={this.state.noun1} onChange={this.onInputChange}/>
-              <input name="adv"
-                className={advClass} placeholder="adverb" type="text" value={this.state.adv} onChange={this.onInputChange}/>
-              <input name="verb"
-                className={verbClass} placeholder="verb" type="text" value={this.state.verb} onChange={this.onInputChange}/>
-              <input name="noun2"
-                className={noun2Class} placeholder="noun" type="text" value={this.state.noun2} onChange={this.onInputChange}/>
+              {filteredInputs}
             </div>
-
             <div className="PlayerSubmissionForm__submit">
               <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
             </div>
